0c6a2043444dcd047400cc01a995a81a46705e4c
Xavier Tapia <fxtapiar@uce.edu.ec>
2026-02-05 18:44:24 -0500

    Taller 36
    Guardianes

--- src/router/index.js
+++ src/router/index.js
@@ -3,17 +3,64 @@ import HomeView from '../views/HomeView.vue'
 
 const routes = [
   { path: '/', name: 'home', component: HomeView },
-  { path: '/consultar', component: () => import('../components/ConsultarTodos.vue') },
-  { path: '/consultar-id', component: () => import('../components/ConsultarPorId.vue') },
-  { path: '/guardar', component: () => import('../components/GuardarEstudiante.vue') },
-  { path: '/actualizar', component: () => import('../components/ActualizarEstudiante.vue') },
-  { path: '/eliminar', component: () => import('../components/EliminarEstudiante.vue') },
-  { path: '/actualizar-parcial', component: () => import('../components/ActualizarParcial.vue') }
+  { path: '/consultar', component: () => import('../components/ConsultarTodos.vue'),
+    meta: {
+    requiereAutorizacion: false,
+    esPublica: true
+    } 
+   },
+  { path: '/consultar-id', component: () => import('../components/ConsultarPorId.vue'),
+    meta: {
+    requiereAutorizacion: false,
+    esPublica: true
+    } 
+   },
+  { path: '/guardar', component: () => import('../components/GuardarEstudiante.vue'),
+    meta: {
+    requiereAutorizacion: false,
+    esPublica: true
+    } 
+   },
+  { path: '/actualizar', component: () => import('../components/ActualizarEstudiante.vue'),
+    meta: {
+    requiereAutorizacion: true,
+    esPublica: false
+    } 
+   },
+  { path: '/eliminar', component: () => import('../components/EliminarEstudiante.vue'), 
+    meta: {
+    requiereAutorizacion: true,
+    esPublica: false
+    } 
+  },
+  { path: '/actualizar-parcial', component: () => import('../components/ActualizarParcial.vue'),
+    meta: {
+    requiereAutorizacion: true,
+    esPublica: false
+    }
+  }
 ]
-
 const router = createRouter({
   history: createWebHashHistory(),
   routes
 })
+/*Configuracion del Guardian*/
+/*to: es a la pagina que estoy intentando acceder
+from: de donde estoy viniendo, desde que pagina vengo
+next: es una variable que me permite decir si le permito irse a la pagina o si le redirecciono 
+avance a la pagina o no avance a la pagina*/
+router.beforeEach((to, from, next) => {
+  if(to.meta.requiereAutorizacion){
+    /*le envio a una pagina de login*/
+    console.log("Requiere autorizacion")
+
+  }else{
+    /*le dejo sin validaciones*/
+    console.log("Pase libre")
+    next()
+  }
+})
+
+
 
 export default router
